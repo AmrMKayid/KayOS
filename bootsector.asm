2d70ea6fe03684dc465ec7c3ef1040f98ff3b2eb
Amr M. Kayid <amrkayid2027@gmail.com>
2018-05-16 15:07:19 +0200

    Say Hello from bootsector

--- bootsector.asm
+++ bootsector.asm
@@ -1,6 +1,20 @@
 ; Infinite loop (e9 fd ff)
-loop:
-	jmp loop
+;loop:
+;	jmp loop
+
+mov ah, 0x0e	; tty mode
+mov al, 'H'
+int 0x10		; interupt
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+int 0x10		; repeated 'l' 
+mov al, 'o'
+int 0x10
+
+
+jmp $ 			; jump to current address => infinite loop
 
 
 ; Fill with 510 zeros minus the size of the previous code
